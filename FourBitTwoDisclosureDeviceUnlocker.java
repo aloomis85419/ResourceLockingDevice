c5f3562a0dc1040751786116a34f8b47170825d0
aloomis85419 <aloomis85419@gmail.com>
2017-09-07 11:40:31 -0600

    DeviceUnlocker and FourBitTwoDiclosureUnlocker skeleton classes created in the repository.

---
+++ FourBitTwoDisclosureDeviceUnlocker.java
@@ -0,0 +1,15 @@
+/**
+ * Created by aloom on 9/7/2017.
+ */
+public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker  {
+
+    /**
+     * Unlocks a resource controlled by a 4-bit/2-disclosure device.
+     * @param dev: the device controlling the resource to unlock; must be a 4-bit device with 2 peek/poke bits.
+     * @return: true if the resource is successfully unlocked (all bits are now identical); false otherwise
+     */
+    public  static boolean unlock(Device dev)
+    {
+
+    }
+}
