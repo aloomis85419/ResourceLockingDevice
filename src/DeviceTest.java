554f9839ffe9101575b7a63c1f183b16e1b27a72
aloomis85419 <aloomis85419@gmail.com>
2017-09-09 16:03:15 -0600

    "Test Classes added. Small changes added to the various classes."

---
+++ src/DeviceTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class for unit testing various aspects of our device class
+ * Created by aloom on 9/9/2017.
+ */
+public class DeviceTest {
+    @Test
+    public void testDeviceCreation() {
+
+    }
+
+    @Test
+    public void testSpin() {
+
+    }
+
+    @Test
+    public void testPeek() {
+
+    }
+
+    @Test
+    public void testPoke() {
+
+    }
+
+    @Test
+    public void testToString() {
+
+    }
+}
---
+++ src/FourBitTwoDisclosureDeviceUnlockerTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by aloom on 9/9/2017.
+ */
+public class FourBitTwoDisclosureDeviceUnlockerTest {
+
+    @Test
+    public void testUnlock() {
+
+    }
+
+    @Test
+    public void testTrace() {
+
+    }
+}
