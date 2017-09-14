e1a37141f1b30e1f0225316c837b1a1fc950315d
aloomis85419 <aloomis85419@gmail.com>
2017-09-14 13:36:58 -0600

    Implementation Added
    Should compile on command line.

--- src/FourBitTwoDisclosureDeviceUnlocker.java
+++ src/FourBitTwoDisclosureDeviceUnlocker.java
@@ -3,6 +3,12 @@
  */
 public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker
 {
+    static private final int MAX_SPIN_COUNT = 10000;
+    static private final String PATTERN = "??--";
+    static private String pokePattern = "TT--";
+    private static char unlockValue = 'T';
+    private static int spinCount = 0;
+
     /**
      * Unlocks a resource controlled by a 4-bit/2-disclosure device.
      * @param dev: the device controlling the resource to unlock; must be a 4-bit device with 2 peek/poke bits.
@@ -10,7 +16,54 @@ public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker
      */
     public static boolean unlock(Device dev)
     {
-        return true;
+        boolean unlocked = init(dev);
+
+        if (!unlocked) {
+            while ((spinCount < MAX_SPIN_COUNT) && (!unlocked)) {
+                unlocked = dev.spin();
+                spinCount++;
+                log("spin(): spincount = " + spinCount);
+                if (unlocked == false) {
+                    String peekResults = (String) dev.peek(PATTERN);
+                    log("peek() peekResults: " + peekResults);
+                    // (peekResults.charAt(0) == peekResults.charAt(1)) : First two chars are equal
+                    // (peekResults.charAt(0) == unlockValue): First char equals unlockValue
+                    if ((peekResults.charAt(0) == peekResults.charAt(1)) && (peekResults.charAt(0) == unlockValue)) {
+                        break; // Don't do anything.
+                    } else {
+                        dev.poke(pokePattern);
+                        log("poke(): " + pokePattern);
+                    }
+                }
+            }
+        }
+        return unlocked;
     }
 
+    private static boolean init(final Device initDevice) {
+        boolean unlockStatus = initDevice.spin();
+        spinCount++;
+        log("spin(): spinCount = " + spinCount);
+
+        if (unlockStatus == true) {
+            return true;
+        } else {
+            CharSequence initialPattern = initDevice.peek("??--");
+            log("peek(): [??--].");
+
+            if (initialPattern.equals("FF--")) {
+                toggleUnlockValue();
+                pokePattern = "FF--";
+            }
+            return false;
+        }
+    }
+
+    private static void toggleUnlockValue() {
+        if (unlockValue == 'T') {
+            unlockValue = 'F';
+        } else if (unlockValue == 'F') {
+            unlockValue = 'T';
+        }
+    }
 }
