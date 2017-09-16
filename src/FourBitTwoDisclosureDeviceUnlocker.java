a04751e852a867a27ecd9ec4936c0a9f2782ba5b
MtnGirl <vlymvw@gmail.com>
2017-09-16 01:41:17 -0600

    Cleanup of this file

--- src/FourBitTwoDisclosureDeviceUnlocker.java
+++ src/FourBitTwoDisclosureDeviceUnlocker.java
@@ -1,18 +1,28 @@
 /**
- * Class extends the DeviceUnlocker class. This class uses a size of 4 bits and a disclosure of 2 bits.
+ * Solution development for 4-bit/2-disclosure device.
+ *
+ * @author Erin Gurnett
+ * @author Aaron Loomis
+ * @author Vicky Lym
+ * @author Joseph Medina
+ * @author Jaziel Pauda
+ * @author Dr. Jody Paul (API)
+ * @version 4.1.4
  */
 public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker
 {
-    private static final int MAX_SPIN_COUNT = 10000;
+    private static final int MAX_SPIN_COUNT = 1000;
     private static final String PATTERN = "??--";
     private static String pokePattern = "TT--";
     private static char unlockValue = 'T';
     private static int spinCount = 0;
+    private static String traceValue = "";
 
     /**
-     * Unlocks a resource controlled by a 4-bit/2-disclosure device.
-     * @param dev: the device controlling the resource to unlock; must be a 4-bit device with 2 peek/poke bits.
-     * @return: true if the resource is successfully unlocked (all bits are now identical); false otherwise
+     * Unlocks a resource controlled by a 4-bit/2-disclosure device. Behavior is unspecified if parameter is
+     * not a reference to a valid 4-bit/2-disclosure device.
+     * @param dev the device controlling the resource to unlock; must be a 4-bit device with 2 peek/poke bits.
+     * @return true if the resource is successfully unlocked (all bits are now identical); false otherwise
      */
     public static boolean unlock(Device dev)
     {
@@ -26,6 +36,7 @@ public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker
                 if (unlocked == false) {
                     String peekResults = (String) dev.peek(PATTERN);
                     log("peek() peekResults: " + peekResults);
+                    
                     // (peekResults.charAt(0) == peekResults.charAt(1)) : First two chars are equal
                     // (peekResults.charAt(0) == unlockValue): First char equals unlockValue
                     if ((peekResults.charAt(0) == peekResults.charAt(1)) && (peekResults.charAt(0) == unlockValue)) {
@@ -37,41 +48,58 @@ public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker
                 }
             }
         }
+        if (spinCount >= MAX_SPIN_COUNT) {
+            log("Maximum number of Spins reached, " + spinCount);
+            log("Program has halted without an unlocked Device, " + unlocked);
+        } else {
+            log("Device is Unlocked.");
+        }
+        traceValue = showTrace();
+        System.out.println("Trace Value is: ");
+        System.out.println(traceValue);
+        traceValue = "";
+        spinCount = 0;
+        log(null);
         return unlocked;
     }
 
+    /**
+     * Helper method to check if device starts out unlocked.
+     * Method modifies poke pattern based on results of first peek.
+     *
+     * @param initDevice Device passed to unlock method
+     * @return true if initDevice is unlocked, false if device is locked
+     */
     private static boolean init(final Device initDevice) {
         boolean unlockStatus = initDevice.spin();
         spinCount++;
         log("spin(): spinCount = " + spinCount);
 
         if (unlockStatus == true) {
-            return true;
+            log("With initial spin(), Device is unlocked.");
         } else {
             CharSequence initialPattern = initDevice.peek("??--");
-            log("peek(): [??--].");
+            log("peek(): [??--], initial Pattern " + initialPattern);
 
             if (initialPattern.equals("FF--")) {
-                toggleUnlockValue();
+                unlockValue = 'F';
                 pokePattern = "FF--";
             }
-            return false;
+            initDevice.poke(pokePattern);
+            log("poke(): " + pokePattern);
         }
+        return unlockStatus;
     }
 
-    private static void toggleUnlockValue() {
-        if (unlockValue == 'T') {
-            unlockValue = 'F';
-        } else if (unlockValue == 'F') {
-            unlockValue = 'T';
-        }
-    }
-
-    public static void main(String[]args)
-    {
-
-       unlock(new Device(4,2));
-
-
-    }
+    // private static void toggleUnlockValue() {
+    //    if (unlockValue == 'T') {
+    //        unlockValue = 'F';
+    //    } else if (unlockValue == 'F') {
+    //        unlockValue = 'T';
+    //    }
+    // }
+    // public static void main(String[]args)
+    // {
+    //    unlock(new Device(4,2));
+    // }
 }
