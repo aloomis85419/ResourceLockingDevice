705ea6ab40aa38f90e9d2dc64be3e2429b239e7b
Jaziel Pauda <jazielpauda@outlook.com>
2017-09-15 23:13:26 -0600

    Added javacdoc comments to FourBit class

--- src/FourBitTwoDisclosureDeviceUnlocker.java
+++ src/FourBitTwoDisclosureDeviceUnlocker.java
@@ -1,5 +1,13 @@
 /**
- * Class extends the DeviceUnlocker class. This class uses a size of 4 bits and a disclosure of 2 bits.
+ * Solution development for 4-bit/2-disclosure device.
+ *
+ * @author Erin Gurnett
+ * @author Aaron Loomis
+ * @author Vicky Lym
+ * @author Joseph Medina
+ * @author Jaziel Pauda
+ * @author Dr. Jody Paul (API)
+ * @version 4.1.4
  */
 public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker
 {
@@ -11,8 +19,9 @@ public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker
 
     /**
      * Unlocks a resource controlled by a 4-bit/2-disclosure device.
+	 *
      * @param dev: the device controlling the resource to unlock; must be a 4-bit device with 2 peek/poke bits.
-     * @return: true if the resource is successfully unlocked (all bits are now identical); false otherwise
+     * @return true if the resource is successfully unlocked (all bits are now identical); false otherwise
      */
     public static boolean unlock(Device dev)
     {
@@ -40,6 +49,13 @@ public class FourBitTwoDisclosureDeviceUnlocker extends DeviceUnlocker
         return unlocked;
     }
 
+	/**
+	 * Helper method to check if device starts out unlocked.
+	 * Method also modifies poke pattern based on results of first peek.
+	 *
+	 * @param initDevice Device passed to unlock method
+	 * @return true if initDevice is unlocked, false if device is locked
+	 */
     private static boolean init(final Device initDevice) {
         boolean unlockStatus = initDevice.spin();
         spinCount++;
